a7585dc5053197189a84b8e5143ee6ac2bc3286a
sc01337 <86912880+sc01337@users.noreply.github.com>
2021-07-04 12:38:01 +0200

    + Alterscheck
    Umbearbeitung vom Alterschecl

---
+++ alterscheck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace langeweile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alter = 18;
+            alter = 18;
+            if (alter >= 18)
+            {
+                Console.WriteLine("Du bist älter als 18.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Du bist keine 18 Jahre alt.");
+                Console.ReadKey();
+            }
+            }
+        }
+    }
